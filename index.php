176ce4efa49bd8fd3325212c0cbfd1b480589f6c
KOVU9529 <bapgiordano@gmail.com>
2022-07-24 17:26:39 +0200

    miglioramento struttura + dettagli

--- index.php
+++ index.php
@@ -19,16 +19,21 @@ $censored=str_replace($variabileGet ,'***', $variabile);
 //
 $variabileLengthMin=strlen($censored);
 ?>
+<h2>Paragrafo originale</h2>
 <p><?php
 echo $variabile;?></p>
+<h3>Lunghezza paragrafo</h3>
 <span> <?php
 echo $variabileLength;?></span>
+<h4>Parola cercata</h4>
 <span><?php
 echo $variabileGet;?></span>
+<h2>Paragrafo censurato</h2>
 <span>
     <?php
     echo $censored;?>
 </span>
+<h3>Lunghezza paragrafo</h3>
 <span>
     <?php
     echo $variabileLengthMin?>
